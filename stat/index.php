f81fcef3f0b9702aff7b1a428e98a8aa2137f573
Mostafa <mostafamahmoudabdelalim@hotmail.com>
2019-09-30 19:58:07 +0200

    Added Stat Commit

---
+++ stat/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$page_title = "404 - Not Found";
+$msg = "The page you are looking for not found or you may don't have permission to see this page";
+$emoji = ":(";
+$msg_header = "404 - Not Found";
+
+require '../config/errorPage.php';
+
+?>
